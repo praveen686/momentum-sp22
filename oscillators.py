035855ea830ee802760df5eb4973fded3b6d7cde
Daniel Bohen <bohen@usc.edu>
2022-02-27 16:51:48 -0800

    Create oscillators.py

---
+++ oscillators.py
@@ -0,0 +1,17 @@
+#momentum oscillators for the input stock symbol over the input period 
+class Oscillators(symbol, period):
+
+    def getRelativeStrengthIndex(self, data):
+        pass
+    
+    def getStochasticOscillator(self, data): 
+        pass
+    
+    def getRateofChange(self, data): 
+        pass
+    
+    def getMovingAverage(self, data):
+        pass
+    
+    def getBollingerBands(self, data):
+        pass
