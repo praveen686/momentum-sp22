4d4f5d8da18ec711eccafdffb4af66b992232031
Daniel Bohen <bohen@usc.edu>
2022-02-27 16:52:22 -0800

    Create indicators.py

---
+++ indicators.py
@@ -0,0 +1,22 @@
+#non-oscillator indicators 
+class Indicators(symbol, period):
+    def getChannelCommodityIndex(self, data):
+        return self.CCI(symbol, period)
+    
+    def getSuperTrend(self, data, multiplier):
+        return self.STR(symbol, period, multiplier)
+    
+    def getVolumeSpike(self, data):
+        if(self.RDV(symbol, period) > 2):
+            return self.RDV(symbol, period)
+        else:
+            return 1
+    
+    def getMoneyFlowIndex(self, data):
+        return self.MFI(symbol, period)
+    
+    def getVWAP(self, data):
+        return self.VWAP(symbol, period)
+    
+ #   Candlestick Indicators
+
